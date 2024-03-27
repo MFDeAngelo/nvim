a55a938ae434d1907bde3405f16ff5c35f225cdf
DeAngelo <matthew.deangelo@qpprograms.com>
2024-03-27 06:48:24 -0400

    switch to kickstart

--- lua/deangelo/init.lua
+++
@@ -1,8 +0,0 @@
-require("deangelo.packer")
-require("deangelo.keymaps")
-require("deangelo.options")
-require("deangelo.telescope")
-require("deangelo.colorscheme")
-require("deangelo.omnisharp")
-
--- require("deangelo.telescope")
